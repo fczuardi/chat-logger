6f571288ab4c4e10c71d2a7d5ea631e0bf07e541
Fabricio C Zuardi <fabricio@fabricio.org>
2016-03-07 16:55:55 -0300

    minor

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ test('array spread support', t => {
     let a = [1,2,3,4],
         b = [5,6,7,8],
         c = [...a, ...b];
-    t.same(c, [ 1, 2, 3, 4, 5, 6, 7, 8 ]);
+    t.same(c, [1,2,3,4,5,6,7,8]);
 });
 
 test('object spread support', async t => {
