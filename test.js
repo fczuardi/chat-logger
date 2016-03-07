07e5662cb4bdd91a6f1fbb66337565c2482faf09
Fabricio C Zuardi <fabricio@fabricio.org>
2016-03-07 13:40:59 -0300

    rewrite tests using the AVA tool

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-//test array spread support
-let a = [1,2,3,4];
-let b = [5,6,7,8];
-console.log([...a, ...b]); // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+import test from 'ava';
 
-//test object spread support
-let c = {a:'foo'};
-let d = {b:'bar'};
-let e = {...c, ...d};
-console.log(e); // { a: 'foo', b: 'bar' }
+test('array spread support', t => {
+    let a = [1,2,3,4],
+        b = [5,6,7,8],
+        c = [...a, ...b];
+    t.same(c, [ 1, 2, 3, 4, 5, 6, 7, 8 ]);
+});
+
+test('object spread support', async t => {
+    let a = {a:'foo'},
+        b = {b:'bar'},
+        c = {...a, ...b};
+    t.same(c, { a: 'foo', b: 'bar' });
+});
